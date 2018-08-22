c51c0dce0eb0ff5aaf9de9f6aeb8ec1546c22542
hartx8 <hartx8@ex.ua>
2018-08-22 23:44:21 +0300

    update with three pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
-  root 'users#index'
+  get 'foo/bar'
+  get 'foo/bar'
+  root 'application#hello'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get  'static_pages/about'
+#  resources :microposts
+#  resources :users
+#  root 'users#index'
 end
