19c00405d3d524e02dfa80f11ef38cabfa430e00
hartx8 <hartx8@ex.ua>
2018-08-26 19:37:09 +0300

    just saving

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,11 +11,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
      assert_response :success
    end
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-  end
+   test "should get home" do
+      get static_pages_home_url
+      assert_response :success
+      assert_select "title", "Ruby on Rails Tutorial Sample App"
+    end
 
   test "should get help" do
     get static_pages_help_url
