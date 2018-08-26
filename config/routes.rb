a3dc6cb05c56bcb19e0ef8ce1605ef131072e61a
hartx8 <hartx8@ex.ua>
2018-08-26 18:29:11 +0300

    Finished 3 chapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'foo/bar'
   get 'foo/bar'
-  root 'application#hello'
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get  'static_pages/about'
